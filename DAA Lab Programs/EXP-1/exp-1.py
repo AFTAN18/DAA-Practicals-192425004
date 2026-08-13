c34cce9e1fc8ab302d568733c408b7dde95c32af
AFTAN18 <aftanfaiz@gmail.com>
2026-08-13 11:43:55 +0530

    Delete DAA Lab Programs directory

--- DAA Lab Programs/EXP-1/exp-1.py
+++
@@ -1,8 +0,0 @@
-words = input("Enter words separated by space: ").split()
-
-for word in words:
-    if word == word[::-1]:
-        print(word)
-        break
-else:
-    print("")
--- DAA Lab Programs/EXP-5/exp-5.py
+++
@@ -1,9 +0,0 @@
-arr = list(map(int, input("Enter array: ").split()))
-
-m = arr[0]
-
-for i in arr:
-    if i > m:
-        m = i
-
-print(m)
